ff809964293759967e0a9c7a108df9c114489da2
codequick <mtspbr@gmail.com>
2015-06-23 11:45:46 -0300

    #1 Add export for sql using lib for windows network authentication

---
+++ docs/export-sql.bat
@@ -0,0 +1,2 @@
+rem java -cp "drivers\postgresql-9.3-1102-jdbc41.jar" -jar codequick.jar -export
+java -Djava.library.path="C:/proj/websphereresources/was/install/mssqlserver/v402206/auth/x64" -jar codequick.jar -export
--- docs/install.bat
+++ docs/install.bat
@@ -1,4 +1,5 @@
 copy .\export.bat \Projects\Codequick\export.bat 
+copy .\export.bat \Projects\Codequick\export-sql.bat 
 copy .\build.bat \Projects\Codequick\build.bat 
 copy ..\target\codequick-1.0.0-jar-with-dependencies.jar \Projects\Codequick\codequick.jar 
 xcopy /E /I ..\templates \Projects\Codequick\templates
