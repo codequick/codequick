64f480740ab59ea2f424a2b270b434059b5d0c9c
mtorre94 <mtorre94@WGC100CTNMJ82.saobernardo.ford.com>
2016-08-10 16:20:30 -0300

    #1 Corrections

--- docs/export-sql.bat
+++ docs/export-sql.bat
@@ -1,6 +1,8 @@
 rem java -cp "drivers\postgresql-9.3-1102-jdbc41.jar" -jar codequick.jar -export
 echo Export will be run with parameter: %1
 
+@ECHO OFF
+
 if %1.==. goto no_config
 
 java -Djava.library.path="C:/proj/websphereresources/was/install/mssqlserver/v402206/auth/x64" -jar codequick.jar -export -config=%1
@@ -8,6 +10,7 @@ java -Djava.library.path="C:/proj/websphereresources/was/install/mssqlserver/v40
 goto end
 
 :no_config
+
 java -Djava.library.path="C:/proj/websphereresources/was/install/mssqlserver/v402206/auth/x64" -jar codequick.jar -export
 
 :end
--- docs/export.bat
+++ docs/export.bat
@@ -1,6 +1,8 @@
 rem java -cp "drivers\postgresql-9.3-1102-jdbc41.jar" -jar codequick.jar -export
 echo Export will be run with parameter: %1
 
+@ECHO OFF
+
 if %1.==. goto no_config
 
 java -jar codequick.jar -export -config=%1
